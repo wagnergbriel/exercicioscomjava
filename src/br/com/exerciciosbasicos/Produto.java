93430bc97bbfc011837e115e9e794ce8a2ee6efa
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-09 12:31:29 -0300

    feat: adicionar conclusao da classe Produto.java

--- src/br/com/exerciciosbasicos/Produto.java
+++ src/br/com/exerciciosbasicos/Produto.java
@@ -1,10 +1,31 @@
 package br.com.exerciciosbasicos;
+import java.util.Scanner;
 
 public class Produto {
     public static void main(String[] args) {
         /*
-            Informar valor, quantidade e o código de 2 peças e do valor da porcentagem do IPI.
-            Retornar o valor total a ser pago, apresentando o resultado.
+            Informar valor, quantidade e o código de 2 produtos e do valor da porcentagem do ipi.
+            Retornar o valor total a ser pago, apresentando o valorTotaldoProduto.
         */
+        float valorDoProduto;
+        float quantidadeDeProduto;
+        float codigoDoProduto;
+        float ipi = 5f;
+        float valorParcialDoProduto = 0;
+        float valorTotaldoProduto = 0;
+        Scanner scanner = new Scanner(System.in);
+        
+        for(int i = 0; i < 2; i++){
+            System.out.println("Informe o codigo do produto:");
+            codigoDoProduto = scanner.nextFloat();
+            System.out.println("Informe o valor do produto:");
+            valorDoProduto = scanner.nextFloat();
+            System.out.println("Informe a quantidade do produto:");
+            quantidadeDeProduto = scanner.nextFloat();
+
+            valorParcialDoProduto += (valorDoProduto * quantidadeDeProduto);
+        }
+        valorTotaldoProduto = valorParcialDoProduto * (ipi/100f + 1f);
+        System.out.printf("\nValor Total do Produto: %.2f\n", valorTotaldoProduto);
     }
 }
