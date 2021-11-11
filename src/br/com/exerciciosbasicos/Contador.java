8dc1dcbf8063de4f77707f5f6691fbc01a8fd9c7
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-11 18:29:45 -0300

    feat: adicionar classes Contador e Salario

--- src/br/com/exerciciosbasicos/Contador.java
+++ src/br/com/exerciciosbasicos/Contador.java
@@ -1,11 +1,23 @@
 package br.com.exerciciosbasicos;
 
+import java.util.Scanner;
+
 public class Contador {
     /*
-        Informar um número inteiro e apresentar o seu
-        antecessor e sucessor.
-    */
+     * Informar um número inteiro e apresentar o seu antecessor e sucessor.
+     */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int numero;
+        int antecessor;
+        int sucessor;
+
+        System.out.println("Digite o número:");
+        numero = scanner.nextInt();
+
+        antecessor = numero - 1;
+        sucessor = numero + 1;
+
+        System.out.printf("antecessor: %d\nAtual posição:%d\nSucessor:%d\n", antecessor, numero, sucessor);
     }
 }
--- src/br/com/exerciciosbasicos/Salario.java
+++ src/br/com/exerciciosbasicos/Salario.java
@@ -1,12 +1,25 @@
 package br.com.exerciciosbasicos;
 
+import java.util.Scanner;
+
 public class Salario {
     /*
-        Informar o salário mínimo e máximo de um usuário.
-        Retornar a quantidade de salários mínimos do usuário.
-    */
+     * Informar o salário mínimo e máximo de um usuário. Retornar a quantidade de
+     * salários mínimos do usuário.
+     */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         float salarioMinimo;
         float salarioMaximo;
+        float qtdDeSalarioMinimo;
+
+        System.out.println("Digite o salário mínimo:");
+        salarioMinimo = scanner.nextFloat();
+        System.out.println("Digite o salário Máximo:");
+        salarioMaximo = scanner.nextFloat();
+
+        qtdDeSalarioMinimo = salarioMaximo / salarioMinimo;
+        System.out.printf("Salário mínimo: %.1f\nSalário máximo: %.1f\nQuantidade de salário mínimo é %.1f\n",
+                salarioMinimo, salarioMaximo, qtdDeSalarioMinimo);
     }
 }
