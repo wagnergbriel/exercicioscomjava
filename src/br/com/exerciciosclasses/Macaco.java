23cd3aa4a2dfae9a942a189c4050424624669e86
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-20 17:05:53 -0300

    feat: adicionar classe Macaco com metodos comer() e verBucho()

---
+++ src/br/com/exerciciosclasses/Macaco.java
@@ -0,0 +1,54 @@
+package br.com.exerciciosclasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Macaco {
+    private String nome;
+    private String bucho;
+    private List<String> estomago = new ArrayList<>();
+
+    public Macaco(String nome) {
+        this.setNome(nome);
+        this.bucho = "Vazio";
+
+    }
+
+    public void comer(String comida) {
+        estomago.add(comida);
+    }
+
+    public void verBucho() {
+        int qtdDeComida = 0;
+        for (int i = 0; i < estomago.size(); i++) {
+            qtdDeComida += 1;
+        }
+
+        if (qtdDeComida > 0 && qtdDeComida <= 3) {
+            setBucho("Satisfeito");
+            System.out.println("O macaco comeu " + estomago + " e o seu bucho está " + getBucho());
+
+        } else if (qtdDeComida > 3) {
+            setBucho("Cheio");
+            System.out.println("O macaco comeu " + estomago + " e o seu bucho está " + getBucho());
+        }
+    }
+
+    // Implementar metodo digerir()
+
+    public String getBucho() {
+        return bucho;
+    }
+
+    public void setBucho(String bucho) {
+        this.bucho = bucho;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- src/br/com/exerciciosclasses/Main.java
+++ src/br/com/exerciciosclasses/Main.java
@@ -2,12 +2,9 @@ package br.com.exerciciosclasses;
 
 public class Main {
     public static void main(String[] args) {
-        BichinhoVirtual bv = new BichinhoVirtual("Pou", 1);
-        System.out.println(bv.alterarNome("Tótó"));
-        System.out.println(bv.verificarFome());
-        System.out.println(bv.verificarSaude());
-        System.out.println(bv.verificarSaude());
-        System.out.println(bv.verificarSaude());
-        System.out.println(bv.verificarHumor());
+        Macaco mico = new Macaco("Pretinho");
+        mico.comer("Banana");
+        mico.comer("Maça");
+        mico.verBucho();
     }
 }
