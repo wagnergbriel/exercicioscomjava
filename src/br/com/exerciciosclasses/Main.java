b0e7dbde7e63dd37808d5a77d543fb108a378228
wagnergbriel <wagnergabriel220596@gmail.com>
2021-11-23 19:33:49 -0300

    fix: alterar o nome do objeto da classe BombadeCombustivel

--- src/br/com/exerciciosclasses/Main.java
+++ src/br/com/exerciciosclasses/Main.java
@@ -2,9 +2,9 @@ package br.com.exerciciosclasses;
 
 public class Main {
     public static void main(String[] args) {
-        BombaDeCombustivel carro = new BombaDeCombustivel("gasolina", 6, 200);
-        carro.abastecerPorLitro(20);
-        carro.abastecerPorValor(120);
+        BombaDeCombustivel gasolina = new BombaDeCombustivel("gasolina", 6, 200);
+        gasolina.abastecerPorLitro(20);
+        gasolina.abastecerPorValor(120);
 
     }
 }
